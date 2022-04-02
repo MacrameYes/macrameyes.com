65fe68a0b40d929b112ee39963326e77acf776de
Hopper Gee <hopper.gee@hey.com>
2022-04-02 11:29:48 +0800

    Create User through Devise

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable,
+         :trackable
+
+  validates_presence_of :first_name
+end
