65414d070ebc781e7141b07e9743e20cd1c2fd51
Hopper Gee <hopper.gee@hey.com>
2022-04-02 12:26:35 +0800

    Create User fixtures

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "valid fixtures" do
+    assert_equal 1, User.count
+    admin = users(:admin)
+
+    assert admin.admin?
+    assert_equal "admin@example.com", admin.email
+    assert_equal "Admin", admin.first_name
+    assert_equal "Li", admin.last_name
+  end
+
 end
